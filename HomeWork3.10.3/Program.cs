4b245000dd287e2838f3ca12e341de3dfcd8f7c3
Михайлов Евгений <jackmihailov@outlook.com>
2021-11-01 13:41:42 +0300

    Добавьте файлы проекта.

---
+++ HomeWork3.10.3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HomeWork3._10._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int i = int.Parse(Console.ReadLine());
+            int n = 2;
+            bool statNum = true;
+            while (statNum)
+                {
+                if (i % n == 0 & n<=i)
+                {
+                    statNum = true;
+                    break;
+                }
+                else
+                {
+                    statNum = false;
+                } 
+                    
+                n++;
+                }
+            if(statNum == true)
+            {
+                Console.WriteLine($"число {i} сложное");
+            }
+            else
+            {
+                Console.WriteLine($"число {i} простое");
+            }
+            
+            Console.ReadLine();
+        }
+
+
+    }
+}
